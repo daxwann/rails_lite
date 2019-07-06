c917284a094cae0bf5356445560e5cf04cf418f0
Dax Wann <daxwann@gmail.com>
2019-07-06 12:58:30 -0700

    add ShowExceptions stack trace functions

--- lib/show_exceptions.rb
+++ lib/show_exceptions.rb
@@ -18,7 +18,19 @@ class ShowExceptions
     template_path = File.join(dir_path, "template", "rescue.html.erb")
     template_str = File.read(template_path)
     template_body = ERB.new(template_str).result(binding)
-    
+
     ['500', {'Content-Type' => 'text/html'}, template_body]
+  end
+
+  def show_src_line_num(e)
+    find_top_stack_trace(e)[1].to_i
+  end
+
+  def show_src_filename(e)
+    find_top_stack_trace(e)[0]
+  end
+
+  def find_top_stack_trace(e)
+    e.backtrace.first.split(':')
   end
 end
